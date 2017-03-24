22291ce22fdaf60e674323ded22eed7be60cb859
Toshi7 <ttoshi0719@gmail.com>
2017-03-24 11:19:05 +0900

    ASP.NET Core with Visual Studio 2017

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using movingtotheweb;
+
+namespace supermva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			/*
+        	String name;
+            Console.WriteLine("Hello World!");
+            name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}!");
+			*/
+			var host = new WebHostBuilder()
+			.UseKestrel()
+			.UseStartup<Startup>()
+			.Build();
+			
+			host.Run();
+        }
+    }
+}
